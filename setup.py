31192d4b296bce2b2a57825a743a4f3cb32e5425
gopikrishnangs44 <59282012+gopikrishnangs44@users.noreply.github.com>
2022-06-09 12:27:36 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='IGRApy',
-    version='0.0.1',
+    version='0.0.3',
     author='Gopikrishnan G S',
     distribution='Python wrapper for IGRA',
     packages=['IGRApy'],
